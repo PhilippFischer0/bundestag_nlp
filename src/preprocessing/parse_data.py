ff1650fdaf189ea4242c9dff85afbe9983f42d86
Philipp Fischer <philipp.fischer@js-soft.com>
2025-01-23 18:15:02 +0100

    added some metadata to sitzung and rede

--- src/preprocessing/parse_data.py
+++ src/preprocessing/parse_data.py
@@ -3,7 +3,7 @@ import json
 from pathlib import Path
 
 
-class XMLParser:
+class PlenarprotokollXMLParser:
     def __init__(self):
         self.data = dict()
 
@@ -16,6 +16,13 @@ class XMLParser:
             self.data[file_id] = []
 
         sitzung = root.find("sitzungsverlauf")
+        sitzungsbeginn = sitzung.find("sitzungsbeginn").attrib.get(
+            "sitzung-start-uhrzeit"
+        )
+        sitzungsende = sitzung.find("sitzungsende").attrib.get("sitzung-ende-uhrzeit")
+        self.data[file_id].append(
+            {"sitzungsbeginn": sitzungsbeginn, "sitzungsende": sitzungsende}
+        )
 
         for tagesordnungspunkt in sitzung.findall("tagesordnungspunkt"):
             tagesordnungspunkt_id = tagesordnungspunkt.attrib.get("top-id")
@@ -26,6 +33,23 @@ class XMLParser:
             for rede in tagesordnungspunkt.findall("rede"):
                 rede_id = rede.attrib.get("id")
                 self.data[file_id][-1][tagesordnungspunkt_id].append({rede_id: []})
+
+                if rede.find("p").attrib.get("klasse") == "redner":
+                    redner_paragraph = rede.find("p")
+                    redner_element = redner_paragraph.find("redner")
+                    name = redner_element.find("name")
+                    redner_id = redner_element.attrib.get("id")
+                    redner = {}
+                    for element in name:
+                        redner[str(element.tag)] = (
+                            element.text
+                            if element.text is not None
+                            else element.find("rolle_lang").text
+                        )
+                    self.data[file_id][-1][tagesordnungspunkt_id][-1][rede_id].append(
+                        {"redner": redner}
+                    )
+
                 for text_paragraph in rede:
                     if text_paragraph.attrib.get("klasse") == "redner":
                         redner_element = text_paragraph.find("redner")
@@ -33,6 +57,7 @@ class XMLParser:
                             redner_element is not None
                             and redner_element.tail is not None
                         ):
+
                             text_paragraph = redner_element.tail.strip()
                             rede_paragraph = {redner_element.tag: text_paragraph}
                     else:
@@ -53,6 +78,8 @@ class XMLParser:
             json.dump(self.data, file, indent=4, ensure_ascii=False)
 
 
-test = XMLParser()
+test = PlenarprotokollXMLParser()
+
+test.get_xml_content("data/xml/20202.xml")
 
 test.crawl_directory("data/xml/", "data/out.json")
