85eb691ef05d82fb3e13820d123cfdf53ebf0062
Philipp Fischer <philipp.fischer@js-soft.com>
2025-01-27 14:46:13 +0100

    added comment analysis

--- src/analysis/analyze_data.py
+++ src/analysis/analyze_data.py
@@ -4,19 +4,19 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 
 class DataAnalyzer:
     def __init__(self, data_path: str):
-        with open(data_path, "r", encoding="utf-8") as f:
-            self.data = json.load(f)
+        with open(data_path, "r", encoding="utf-8") as file:
+            self.data = json.load(file)
 
-    def extract_paragraphs(self, data: json, key: str):
+    def extract_paragraphs(self, data: dict | list, key: str):
         paragraphs = []
-        if isinstance(self.data, dict):
-            for key, value in self.data.items():
+        if isinstance(data, dict):
+            for key, value in data.items():
                 if key == key and isinstance(value, str):
                     paragraphs.append(value)
                 else:
                     paragraphs.extend(self.extract_paragraphs(value))
-        elif isinstance(self.data, list):
-            for item in self.data:
+        elif isinstance(data, list):
+            for item in data:
                 paragraphs.extend(self.extract_paragraphs(item))
 
         return paragraphs
@@ -34,7 +34,22 @@ class DataAnalyzer:
     def word_frequency_dist(self, tokens: list):
         word_frequency = nltk.FreqDist(tokens)
 
+        # TODO: remove stopwords
         return word_frequency
 
-    def num_comments_per_speaker_dist(self):
-        pass
+    def num_comments_per_speaker(self, data: dict | list, target_redner: str):
+        comments = 0
+        if isinstance(data, dict):
+            for key, value in data.items():
+                comments += self.num_comments_per_speaker(value, target_redner)
+        elif isinstance(data, list):
+            for item in data:
+                if item.get("redner") == target_redner:
+                    for item in data:
+                        if list(item.keys())[0] == "kommentar":
+                            comments += 1
+                else:
+                    for key, value in item.items():
+                        comments += self.num_comments_per_speaker(value, target_redner)
+
+        return comments
