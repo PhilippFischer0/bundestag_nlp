502481960d8788220c9a8dbc753e650b74f9322f
Philipp Fischer <philipp.fischer@js-soft.com>
2025-02-03 16:19:42 +0100

    implementation for downloading xml files

--- 01_scrape_data.py
+++ 01_scrape_data.py
@@ -1,6 +1,7 @@
 import os
+from src.fetch_data.get_data import scrape_data
 
 if not os.path.exists("data/") and not os.path.isdir("data/"):
     os.makedirs(os.getenv("XML_PATH"))
-else:
-    pass
+
+scrape_data()
--- src/fetch_data/get_data.py
+++ src/fetch_data/get_data.py
@@ -1,13 +1,41 @@
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
+import requests
+from requests import JSONDecodeError
+from bs4 import BeautifulSoup
+import os
+import time, math
 
-try:
-    geckodriver_path = "/snap/bin/geckodriver"
-    driver_service = webdriver.FirefoxService(executable_path=geckodriver_path)
-    driver = webdriver.Firefox(service=driver_service)
-    driver.get("https://www.bundestag.de/services/opendata")
-    html = driver.page_source
-    print(html[:100])
-    driver.quit()
-except WebDriverException as e:
-    print(e)
+
+def scrape_data() -> None:
+
+    base_url = "https://www.bundestag.de/ajax/filterlist/de/services/opendata/866354-866354?limit=10&noFilterSet=true&offset={offset}"
+
+    offset = 0
+
+    # get maximum number of XML Documents
+    data_hits = int(
+        BeautifulSoup(
+            requests.get(base_url.format(offset=offset))._content, "html.parser"
+        )
+        .find("div")
+        .get("data-hits")
+    )
+
+    while offset < data_hits:
+        url = base_url.format(offset=offset)
+        response = requests.get(url=url)
+        try:
+            soup = BeautifulSoup(response._content, "html.parser")
+            for link in soup.find_all("a"):
+                href = link.get("href")
+                if href and href.endswith(".xml"):
+                    xml_response = requests.get(href)
+                    xml_file_name = href.split("/")[-1]
+                    with open(
+                        f"{os.getenv("XML_PATH")}/{xml_file_name}", "wb"
+                    ) as xml_file:
+                        xml_file.write(xml_response.content)
+                time.sleep(5)
+        except JSONDecodeError as e:
+            print(e)
+
+        offset += 10
