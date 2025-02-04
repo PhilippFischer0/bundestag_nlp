6d85ab927a4992e25a0c3c751e0d0c7edf00a15a
Philipp Fischer <philipp.fischer@js-soft.com>
2025-02-04 11:57:57 +0100

    added PR requested changes

--- src/fetch_data/get_data.py
+++ src/fetch_data/get_data.py
@@ -1,41 +1,43 @@
 import requests
-from requests import JSONDecodeError
+
 from bs4 import BeautifulSoup
 import os
-import time, math
+import time
+import re
 
 
 def scrape_data() -> None:
 
-    base_url = "https://www.bundestag.de/ajax/filterlist/de/services/opendata/866354-866354?limit=10&noFilterSet=true&offset={offset}"
+    url = "https://www.bundestag.de/ajax/filterlist/de/services/opendata/866354-866354"
 
-    offset = 0
+    payload = {
+        "limit": 10,
+        "FilterSet": "true",
+        "offset": 0,
+    }
 
     # get maximum number of XML Documents
     data_hits = int(
-        BeautifulSoup(
-            requests.get(base_url.format(offset=offset))._content, "html.parser"
-        )
-        .find("div")
+        BeautifulSoup(requests.get(url=url, params=payload)._content, "html.parser")
+        .find("div", class_="meta-slider")
         .get("data-hits")
     )
 
-    while offset < data_hits:
-        url = base_url.format(offset=offset)
-        response = requests.get(url=url)
-        try:
-            soup = BeautifulSoup(response._content, "html.parser")
-            for link in soup.find_all("a"):
-                href = link.get("href")
-                if href and href.endswith(".xml"):
-                    xml_response = requests.get(href)
-                    xml_file_name = href.split("/")[-1]
-                    with open(
-                        f"{os.getenv("XML_PATH")}/{xml_file_name}", "wb"
-                    ) as xml_file:
-                        xml_file.write(xml_response.content)
-                time.sleep(5)
-        except JSONDecodeError as e:
-            print(e)
+    while payload["offset"] < data_hits:
+        response = requests.get(url=url, params=payload)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            for link in soup.find_all("a", attrs={"title": re.compile("^XML")}):
 
-        offset += 10
+                href = link.get("href")
+                xml_response = requests.get(href)
+                xml_file_name = href.split("/")[-1]
+
+                with open(
+                    os.path.join(os.getenv("XML_PATH"), xml_file_name), "wb"
+                ) as xml_file:
+                    xml_file.write(xml_response.content)
+
+                time.sleep(5)
+
+            payload["offset"] += 10
