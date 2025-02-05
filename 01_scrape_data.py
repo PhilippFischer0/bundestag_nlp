9ea5ecf087bcde278b6676d1c699ac70be93d8fa
Philipp Fischer <philipp.fischer@js-soft.com>
2025-02-05 17:29:55 +0100

    added env variables to scripts

--- 01_scrape_data.py
+++ 01_scrape_data.py
@@ -1,7 +1,9 @@
 import os
 from src.fetch_data.get_data import scrape_data
 
-if not os.path.exists("data/") and not os.path.isdir("data/"):
-    os.makedirs(os.getenv("XML_PATH"))
 
-scrape_data()
+if __name__ == "__main":
+    if not os.path.exists("data/") and not os.path.isdir("data/"):
+        os.makedirs(os.getenv("XML_PATH"))
+
+    scrape_data(output_directory_path=os.getenv("XML_PATH"))
--- 03_parse_and_load_data.py
+++ 03_parse_and_load_data.py
@@ -3,8 +3,15 @@ from src.preprocessing.parse_data import PlenarprotokollXMLParser
 from src.sqlite.load_data_into_db import load_data_into_db
 
 
-parser = PlenarprotokollXMLParser()
+if __name__ == "__main__":
+    parser = PlenarprotokollXMLParser()
 
-parser.crawl_directory()
+    parser.crawl_directory(
+        input_directory_path=os.getenv("XML_PATH"),
+        output_directory_path=os.getenv("JSON_PATH"),
+    )
 
-load_data_into_db()
+    load_data_into_db(
+        json_directory_path=os.getenv("JSON_PATH"),
+        database_path=os.getenv("DATABASE_FILEPATH"),
+    )
