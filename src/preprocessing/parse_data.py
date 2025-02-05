6ac0da8b6eb10f892810368c00c04f807ae2abd2
Philipp Fischer <philipp.fischer@js-soft.com>
2025-02-05 17:31:00 +0100

    changed parsing to new data architecture and removed env variables

--- src/preprocessing/parse_data.py
+++ src/preprocessing/parse_data.py
@@ -1,26 +1,30 @@
 import xml.etree.ElementTree as ET
-import json, re
+import json
 from pathlib import Path
 import os
+import re
 
 
 class PlenarprotokollXMLParser:
     def __init__(self):
         self.data = dict()
+        self.redner = dict()
+        self.rollen = dict()
+        self.rollen_counter = 1
 
     # due to the xml-files containing invisible and ambiguous characters they have to be removed
     def remove_bad_chars(self, text: str) -> str:
-        invisible_spaces_pattern = re.compile(r"[\u00A0]")
-        invisible_pattern = re.compile(r"[\u202F]")
-        bad_dashes_pattern = re.compile(r"[\u2013]")
-        bad_quotation_pattern = re.compile(r"[\u201c\u201e]")
+        sub_map = {
+            r"[\u00A0]": " ",
+            r"[\u202F]": "",
+            r"[\u2013]": "-",
+            r"[\u201c\u201e]": "'",
+        }
 
-        cleaned_text = invisible_spaces_pattern.sub(" ", text)
-        cleaned_text = invisible_pattern.sub("", cleaned_text)
-        cleaned_text = bad_dashes_pattern.sub("-", cleaned_text)
-        cleaned_text = bad_quotation_pattern.sub("'", cleaned_text)
+        for pattern, substitute in sub_map.items():
+            text = re.compile(pattern=pattern).sub(substitute, text)
 
-        return cleaned_text
+        return text
 
     def extract_spoken_comments(self, comment: str) -> list[dict]:
         pattern = re.compile(
@@ -32,8 +36,7 @@ class PlenarprotokollXMLParser:
             matches = pattern.findall(segment)
             for match in matches:
                 person, party, text = match
-                if text.endswith(")"):
-                    text = text[:-1].strip()
+                text = text.rstrip(")")
                 extracted_info.append(
                     {
                         "commentator": person.strip(),
@@ -68,24 +71,20 @@ class PlenarprotokollXMLParser:
             "sitzungsende": sitzungsende,
         }
 
-        # iterate through all tagesordnungspunkte in a sitzung and add the to inhalt
+        # iterate through all tagesordnungspunkte in a sitzung
         for tagesordnungspunkt in sitzung.findall("tagesordnungspunkt"):
             tagesordnungspunkt_id = tagesordnungspunkt.attrib.get("top-id")
 
-            if tagesordnungspunkt_id not in self.data[file_id]:
-                self.data[file_id]["inhalt"].update({tagesordnungspunkt_id: {}})
+            tagesordnungspunkt_dict = {tagesordnungspunkt_id: {}}
 
             # iterate through all rede elements and add them to the corresponding tagesordnungspunkt
             for rede in tagesordnungspunkt.findall("rede"):
-                rede_id = rede.attrib.get("id")
-                self.data[file_id]["inhalt"][tagesordnungspunkt_id].update(
-                    {rede_id: {}}
-                )
-
                 # add two lists, one for the text of the speech the other for comments made by other politicians
-                self.data[file_id]["inhalt"][tagesordnungspunkt_id][rede_id].update(
-                    {"text": [], "kommentare": []}
-                )
+                rede_dict = {"text": []}
+                kommentar_dict = {"kommentare": []}
+                reference_dict = {"reference": {}}
+                rede_id = rede.attrib.get("id")
+
                 # initialize a comment counter which is later used as an index
                 comment_counter = 0
 
@@ -96,13 +95,12 @@ class PlenarprotokollXMLParser:
                         text_paragraph.attrib.get("klasse") == "redner"
                         or text_paragraph.tag == "kommentar"
                     ):
+                        # TODO: <name> Elemente + nachfolgende Rede aus Text parsen
                         if text_paragraph.text is not None:
                             rede_paragraph = [
                                 self.remove_bad_chars(text_paragraph.text)
                             ]
-                            self.data[file_id]["inhalt"][tagesordnungspunkt_id][
-                                rede_id
-                            ]["text"].extend(rede_paragraph)
+                            rede_dict["text"].extend(rede_paragraph)
 
                     # else check if it is a comment, if so add it to the list of comments
                     elif text_paragraph.tag == "kommentar":
@@ -110,51 +108,93 @@ class PlenarprotokollXMLParser:
                         comments = self.extract_spoken_comments(text_paragraph)
                         for comment in comments:
                             comment_counter += 1
-                            self.data[file_id]["inhalt"][tagesordnungspunkt_id][
-                                rede_id
-                            ]["kommentare"].append({comment_counter: comment})
+                            kommentar_dict["kommentare"].append(
+                                {comment_counter: comment}
+                            )
+                if rede_id not in tagesordnungspunkt_dict[tagesordnungspunkt_id]:
+                    tagesordnungspunkt_dict[tagesordnungspunkt_id][rede_id] = {}
+
+                tagesordnungspunkt_dict[tagesordnungspunkt_id][rede_id].update(
+                    rede_dict
+                )
+                if len(kommentar_dict["kommentare"]) > 0:
+                    tagesordnungspunkt_dict[tagesordnungspunkt_id][rede_id].update(
+                        kommentar_dict
+                    )
 
                 # extract the redner element from the relevant paragraph
                 if rede.find("p").attrib.get("klasse") == "redner":
                     redner_paragraph = rede.find("p")
                     redner_element = redner_paragraph.find("redner")
-                    # find the name element, which holds the necessary metainformation
-                    name = redner_element.find("name")
                     # get the id from the speaker and add it to th relevant dictionary
                     redner_id = redner_element.attrib.get("id")
                     redner = {"redner_id": redner_id}
+                    # find the name element, which holds the necessary metainformation
+                    name = redner_element.find("name")
 
                     # iterate through the elements in the name element and add them to the redner dictionary
                     for element in name:
-                        redner[str(element.tag)] = (
-                            self.remove_bad_chars(element.text)
-                            if element.text is not None
-                            # check if the redner has a role and if so adds it to the dict
-                            else self.remove_bad_chars(element.find("rolle_lang").text)
-                        )
-                    self.data[file_id]["inhalt"][tagesordnungspunkt_id][rede_id].update(
-                        {"redner": redner}
-                    )
+                        # check if the redner has a role and if so adds it to the dict
+                        if element.tag == "rolle":
+                            # TODO: some roles still have whitespace in them, needs to be removed
+                            rollen_element = element.find("rolle_lang").text
+                            rollen_element = re.compile(r"\n").sub(" ", rollen_element)
+                            rollen_element = re.compile(r" +").sub(" ", rollen_element)
+                            if len(list(self.rollen.values())) == 0:
+                                self.rollen["rollen"] = {}
+                                self.rollen["map"] = {}
+                            if rollen_element not in self.rollen["rollen"].values():
+                                self.rollen["rollen"][self.rollen_counter] = {}
+                                self.rollen["rollen"][
+                                    self.rollen_counter
+                                ] = rollen_element
+                                self.rollen["map"][rollen_element] = {}
+                                self.rollen["map"][rollen_element] = str(
+                                    self.rollen_counter
+                                )
+                                self.rollen_counter += 1
+                        else:
+                            redner[str(element.tag)] = self.remove_bad_chars(
+                                element.text
+                            )
+                    if redner_id not in self.redner.keys():
+                        self.redner[redner_id] = {}
+                        self.redner[redner_id].update(redner)
 
-        # some tagesordnungspunkte don't contain speeches so they are removed here
-        keys_to_remove = []
-        for tagesordnungspunkt_key, tagesordnungspunkt_values in self.data[file_id][
-            "inhalt"
-        ].items():
-            if len(list(tagesordnungspunkt_values)) == 0:
-                keys_to_remove.append(tagesordnungspunkt_key)
+                reference_dict["reference"]["redner"] = redner_id
+                if name.find("rolle") is not None:
+                    reference_dict["reference"]["rolle"] = self.rollen["map"][
+                        rollen_element
+                    ]
 
-        for key in keys_to_remove:
-            self.data[file_id]["inhalt"].pop(key)
+                tagesordnungspunkt_dict[tagesordnungspunkt_id][rede_id].update(
+                    reference_dict
+                )
 
-        return self.data
+            if (
+                tagesordnungspunkt_id not in self.data[file_id]
+                and len(list(tagesordnungspunkt_dict[tagesordnungspunkt_id].values()))
+                > 0
+            ):
+                self.data[file_id]["inhalt"].update(tagesordnungspunkt_dict)
+
+        return self.data, self.redner, self.rollen
 
     # iterate through directory to append the data from each file present into one json file
-    def crawl_directory(self) -> None:
-        pathlist = sorted(Path(os.getenv("XML_PATH")).rglob("*.xml"))
+    def crawl_directory(
+        self, input_directory_path: str, output_directory_path: str
+    ) -> None:
+        pathlist = sorted(Path(input_directory_path).rglob("*.xml"))
 
         for path in pathlist:
-            self.data.update(self.get_xml_content(path))
+            data, redner, rollen = self.get_xml_content(path)
+            self.data.update(data)
+            self.redner.update(redner)
+            self.rollen.update(rollen)
 
-        with open(os.getenv("JSON_FILEPATH"), "a") as file:
+        with open(os.path.join(output_directory_path, "data.json"), "a") as file:
             json.dump(self.data, file, indent=4, ensure_ascii=False)
+        with open(os.path.join(output_directory_path, "redner.json"), "a") as file:
+            json.dump(self.redner, file, indent=4, ensure_ascii=False)
+        with open(os.path.join(output_directory_path, "rollen.json"), "a") as file:
+            json.dump(self.rollen, file, indent=4, ensure_ascii=False)
