bab4007c2f5e722a6d437719bbf68827a9f4e0bf
Philipp Fischer <philipp.fischer@js-soft.com>
2025-02-21 11:31:27 +0100

    formatting

--- 01_scrape_data.py
+++ 01_scrape_data.py
@@ -2,7 +2,6 @@ import os
 
 from src.fetch_data.get_data import scrape_data
 
-
 if __name__ == "__main__":
     if os.path.exists(os.getenv("XML_PATH")) and not os.path.isdir(
         os.getenv("XML_PATH")
